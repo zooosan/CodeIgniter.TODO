e6302cec40eecd31d81bfc0631839e4148bc3f36
zooosan <c.remarks@gmail.com>
2018-05-02 14:27:41 +0900

    index.php設定

--- htdocs/index.php
+++ htdocs/index.php
@@ -97,7 +97,8 @@ switch (ENVIRONMENT)
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-	$system_path = 'system';
+	//$system_path = 'system';
+	$system_path = '../system';
 
 /*
  *---------------------------------------------------------------
@@ -114,7 +115,8 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = 'application';
+	//$application_folder = 'application';
+  $application_folder = '../application';
 
 /*
  *---------------------------------------------------------------
